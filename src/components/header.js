ed71a569c0682d39ad771f329b1ebeaa38d5f323
Ryan Ponce <ryan@ponce.la>
2019-03-17 00:04:44 -0400

    Remove default styling

--- src/components/header.js
+++ src/components/header.js
@@ -4,25 +4,12 @@ import React from "react";
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `gold`,
-      marginBottom: `1.45rem`
-    }}
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
         >
           {siteTitle}
         </Link>
